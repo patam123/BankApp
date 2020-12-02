550e003a482f861e8907821655561763e19a08dc
patam123 <patrik.hammarstrand@gmail.com>
2020-12-02 10:25:31 +0100

    Add classes, fix categorization functionality

---
+++ BankApp/Client/Shared/IndividualTransaction.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankApp.Shared;
+
+namespace BankApp.Client.Shared
+{
+    public partial class IndividualTransaction
+    {
+        public IndividualTransaction()
+        {
+
+        }
+
+        [Parameter] public Transaction Transaction { get; set; }
+        [Parameter] public List<Category> Categories { get; set; }
+
+        public void SetCategory(Transaction transaction, ChangeEventArgs e) // gör private => gör en ny metod att kalla på i .razorkomponent
+        {
+            Category category = Categories.Find(x => x.Name.Equals(e.Value));
+            transaction.Category = category;
+            Console.WriteLine($"{transaction.Name} har lagts till i kategorin \"{category.Name}\"");
+        }
+    }
+}
---
+++ BankApp/Shared/Entities/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApp.Shared
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+
+        }
+        public Transaction(DateTime transactionDate, string name, Category category, decimal sum)
+        {
+            TransactionDate = transactionDate;
+            Name = name;
+            Category = category;
+            Sum = Math.Round(sum, 2);
+        }
+
+        public int Id { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public decimal Sum { get; set; }
+    }
+}
