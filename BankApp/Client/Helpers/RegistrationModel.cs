7e887b84d6715eaac9ccb7dfc6636db7ed08ff28
patam123 <patrik.hammarstrand@gmail.com>
2020-12-09 16:54:11 +0100

    add register and login components + models

---
+++ BankApp/Client/Helpers/RegistrationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Client.Helpers
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 8, ErrorMessage = "Ditt lösenord måste vara 8-25 tecken långt")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 8)]
+        public string RepeatPassword { get; set; }
+
+    }
+}
